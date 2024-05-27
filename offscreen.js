3fc91c9299618ee0ee43d064ff53fe6e3931222e
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2024-05-27 20:56:43 +0900

    Example code to open pip

--- offscreen.js
+++ offscreen.js
@@ -1,7 +1,17 @@
-chrome.runtime.onMessage.addListener(async () => {
-  try {
-    await window.documentPictureInPicture.requestWindow();
-  } catch (e) {
-    alert(e);
-  }
+chrome.runtime.onMessage.addListener(() => {
+  const video = document.createElement("video");
+  video.addEventListener("loadedmetadata", () => {
+    video.requestPictureInPicture();
+  });
+
+  const source = document.createElement("source");
+  source.setAttribute(
+    "src",
+    "https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.webm"
+  );
+  source.setAttribute("type", "video/webm");
+  video.appendChild(source);
+  video.autoplay = true;
+
+  document.body.appendChild(video);
 });
